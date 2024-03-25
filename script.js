9d63c80322bfd42a449fef53cd75383c914a41dd
sruthi393 <sruthivimala919@gmail.com>
2024-03-25 21:41:42 +0530

    init

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
+let show = document.getElementById("show");
+let search = document.getElementById("search");
+let cityVal = document.getElementById("city");
 
- 
-const url =
-'https://api.openweathermap.org/data/2.5/weather';
-const apiKey =
-'f00c38e0279b7bc85480c3fe775d518c';
+//Make sure you have your own key.
+let key = "2f745fa85d563da5adb87b6cd4b81caf";
 
-$(document).ready(function () {
-weatherFn('Pune');
-});
-
-async function weatherFn(cName) {
-const temp =
-    `${url}?q=${cName}&appid=${apiKey}&units=metric`;
-try {
-    const res = await fetch(temp);
-    const data = await res.json();
-    if (res.ok) {
-        weatherShowFn(data);
-    } else {
-        alert('City not found. Please try again.');
-    }
-} catch (error) {
-    console.error('Error fetching weather data:', error);
-}
-}
-
-function weatherShowFn(data) {
-$('#city-name').text(data.name);
-$('#date').text(moment().
-    format('MMMM Do YYYY, h:mm:ss a'));
-$('#temperature').
-    html(`${data.main.temp}°C`);
-$('#description').
-    text(data.weather[0].description);
-$('#wind-speed').
-    html(`Wind Speed: ${data.wind.speed} m/s`);
-$('#weather-icon').
-    attr('src',
-        `...`);
-$('#weather-info').fadeIn();
-}
+let getWeather = () => {
+  let cityValue = cityVal.value;
+  if (cityValue.length == 0) {
+    show.innerHTML = `<h3 class="error">Please enter a city name</h3>`;
+  }
+  else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+    cityVal.value = "";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        show.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <h4 class="weather">${data.weather[0].main}</h4>
+        <h4 class="desc">${data.weather[0].description}</h4>
+        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+        <h1>${data.main.temp} &#176;</h1>
+        <div class="temp_container">
+         <div>
+            <h4 class="title">min</h4>
+            <h4 class="temp">${data.main.temp_min}&#176;</h4>
+         </div>
+         <div>
+            <h4 class="title">max</h4>
+            <h4 class="temp">${data.main.temp_max}&#176;</h4>
+         </div>   
+        </div>
+        `;
+      })
+      .catch(() => {
+        show.innerHTML = `<h3 class="error">City not found</h3>`;
+      });
+  }
+};
+search.addEventListener("click", getWeather);
+window.addEventListener("load", getWeather);
